1d3bcb6d2648cec3e081e0bbcde4f9262020fe49
Devon Schwartz <devon.s.schwartz@gmail.com>
2025-10-24 17:15:40 -0500

    Fixed tests so that now it is just asserts

--- waitlist.py
+++ waitlist.py
@@ -6,10 +6,7 @@ if the username is in waitlist, return the index of that user so that 0 <= index
 if the username is not in the waitlist, return -1
 '''
 def getPosition(username : str, waitlist : list[str]) -> int:
-    for i in range(len(waitlist)):
-        if waitlist[i] == username:
-            return i
-    return -1
+    pass
 
 '''
 Remove the person at the front of the waitlist or return None if the waitlist si empty
@@ -20,9 +17,7 @@ and remove the element at waitlist[0]
 if len(waitlist) == 0 then return None
 '''
 def popFront(waitlist: list[str]) -> str:
-    if len(waitlist) == 0:
-        return None
-    return waitlist.pop(0) 
+    pass
 
 '''
 Add username to the waitlist if getPosition(username) == -1
@@ -34,10 +29,7 @@ if username is in waitlist, return False. len(waitlist) at the end of the functi
 len(waitlist) at the beginning of the function
 '''
 def addUsername(username: str, waitlist : list[str]) -> bool:
-    if getPosition(username, waitlist) == -1:
-        waitlist.append(username)
-        return True
-    return False
+    pass
 
 '''
 Remove the username from waitlist if username is in waitlist and return True if the username
@@ -50,11 +42,7 @@ if the username is in waitlist, waitlist is mutated so that at the end of the fu
 the size of the waitlist is decreased by 1 and username not in waitlist. In addition, return True
 '''
 def removeUsername(username : str, waitlist : list[str]) -> bool:
-    for i in range(len(waitlist)):
-        if waitlist[i] == username:
-            waitlist.pop(i)
-            return True
-    return False
+    pass
 
 
 '''
@@ -71,11 +59,4 @@ len(waitlist) > 0 : return string representation of waitlist
 len(waitlist) == 0: return "Waitlist Empty"
 '''
 def printWaitlist(waitlist: str) -> str:
-    if len(waitlist) == 0:
-        return 'Waitlist Empty'
-
-    output = []
-    for i in range(len(waitlist)):
-        output.append(f"{i + 1}:{waitlist[i]}")
-
-    return '\n'.join(output)
+    pass
