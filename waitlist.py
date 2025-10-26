046a14b47a6740fdae5c772b4ce88e242c9cf776
Devon Schwartz <devon.s.schwartz@gmail.com>
2025-10-26 09:30:47 -0500

    fixed typo

--- waitlist.py
+++ waitlist.py
@@ -9,7 +9,7 @@ def getPosition(username : str, waitlist : list[str]) -> int:
     pass
 
 '''
-Remove the person at the front of the waitlist or return None if the waitlist si empty
+Remove the person at the front of the waitlist or return None if the waitlist is empty
 PreConditions: 0 <= len(waitlist)
 Post Conditions:
 if len(waitlist) > 0 then return the name of the person at waitlist[0] and remove waitlist[0] from waitlist
